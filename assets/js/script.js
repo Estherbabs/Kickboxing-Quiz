7ec9cd42c56cea90e0ace3b01a06bacc8c4b443c
Estherbabs <oluwaseyibaba@gmail.com>
2022-11-06 14:03:43 +0000

    Changed codes for home page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,38 @@
+function myfunction(){
+    var x =document.getElementById("pass");
+
+    if(x.type === "password"){
+        x.type = "text"
+    }
+    else{
+        x.type = "password";
+    }
+}
+
+function validate(){
+    var password = document.getElementById("pass");
+    var length = document.getElementById("length");
+
+    if(password.value.length >= 8){
+        alert("Login Successful");
+        window.location.replace("quiz.html");
+        return false;
+    }
+    else{    
+        alert("Login Failed");
+
+    }
+}
+
+
+
+
 let questions = [
     {
         id: 1,
         question: "Which one of these rules are allowed in Kickboxing?",
         answer: "Kicks below the waist",
-        options: [
+        option: [
             "Kicks above the elbow",
             "Throwing punching in chest",
             "Sweeping opponent to the ground",
@@ -15,7 +44,7 @@ let questions = [
         id: 2,
         question: "Where did Kickboxing originate from?",
         answer: "Southeast Asia",
-        options: [
+        option: [
             "America",
             "Germany",
             "Australia",
@@ -27,7 +56,7 @@ let questions = [
         id: 3,
         question: "Which one is the benefit of Kickboxing?",
         answer: "Self-Defense",
-        options: [
+        option: [
             "More sleep",
             "Hydration",
             "Self-Defense",
@@ -40,7 +69,7 @@ let questions = [
         id: 4,
         question: "Which of these is not a Kickboxing style?",
         answer: "Firestarter",
-        options: [
+        option: [
             "Muay Thai",
             "Firestarter",
             "Full Contact Karate",
@@ -52,7 +81,7 @@ let questions = [
         id: 5,
         question: "Which country does Muay Thai orginates from?",
         answer: "Thailand",
-        options: [
+        option: [
             "Japan",
             "China",
             "Thailand",
@@ -64,7 +93,7 @@ let questions = [
         id: 6,
         question: "Who is the 2022 Heavyweight UFC Champion?",
         answer: "Francis Ngannou",
-        options: [
+        option: [
             "Francis Ngannou",
             "Yoel Romero",
             "Colby Covington",
@@ -77,7 +106,7 @@ let questions = [
         id: 7,
         question: "How many divisions are in the UFC?",
         answer: "12",
-        options: [
+        option: [
             "8",
             "15",
             "11",
@@ -90,7 +119,7 @@ let questions = [
         id: 8,
         question: "When year was the UFC founded?",
         answer: "1993",
-        options: [
+        option: [
             "1993",
             "1972",
             "1985",
@@ -102,7 +131,7 @@ let questions = [
        id: 9,
        question: "Which one is considered a mixed martial arts (MMA)?",
        answer: "Hybrid combat sport",
-       options: [
+       option: [
            "Taekwondo",
            "Karate",
            "Hybrid combat sport",
@@ -114,7 +143,7 @@ let questions = [
         id: 10,
         question: "Which is the main category of Martial arts?",
         answer: "All of the above",
-        options: [
+        option: [
             "Japanese",
             "Chinese",
             "Korean",
@@ -124,32 +153,3 @@ let questions = [
 
 ];
 
-let question_count = 0;
-
-window.onload = function() {
-    let question = document.getElementById("questions");
-    let [first, second, third, fourth] = question[count].options;
-
-    question.innerHTML = `<h2>Q${count + 1}. $(questions[count].question)</h2>
-    <ul class="option_group">
-    <li class="option">$(first)</li>
-    <li class="option">$(second)</li>
-    <li class="option">$(third)</li>
-    <li class="option">$(fourth)</li>
-    </ul>`;
-    toggleActive();
-}
-
-function toggleActive(){
-    let option = document.querySelectorAll("li.option");
-    for(let i=0; i <option.length; i++){
-        option[i].onclick = function() {
-            for(let i=0; i< option.length; 1++){
-                if(option[i].classList.contains("active")){
-                    option[i].classList.remove("active");
-                }
-            }
-            option[i].classList.add("active");
-        }
-    }
-}
