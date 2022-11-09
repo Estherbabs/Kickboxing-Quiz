b9b5a46e978e72fe3a2666a0263bc9c689812cb3
Estherbabs <oluwaseyibaba@gmail.com>
2022-11-09 05:29:38 +0000

    Added some javascript and style to quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,155 +1,236 @@
-function myfunction(){
-    var x =document.getElementById("pass");
 
-    if(x.type === "password"){
-        x.type = "text"
+window.addEventListener("load", function(){
+  const userDetails = JSON.parse(localStorage.getItem("user_details"))
+  console.log(window.location.pathname)
+  if(!userDetails?.password && window.location.pathname === '/quiz.html'){
+    window.location =  'index.html'
+  }
+
+})
+
+function myfunction() {
+    var x = document.getElementById("pass");
+
+    if (x.type === "password") {
+        x.type = "text";
     }
-    else{
+    else {
         x.type = "password";
     }
 }
-
-function validate(){
+function validate() {
+  var username = document.getElementById("username")
     var password = document.getElementById("pass");
     var length = document.getElementById("length");
 
-    if(password.value.length >= 8){
+    var userDetails = {
+     username: username.value,
+     password: password.value
+    }
+
+    if (password.value.length >= 8) {
         alert("Login Successful");
+        localStorage.setItem("user_details", JSON.stringify(userDetails))
         window.location.replace("quiz.html");
         return false;
     }
-    else{    
+    else {
         alert("Login Failed");
 
     }
 }
-
-
-
-
-let questions = [
+const Data = [
     {
-        id: 1,
         question: "Which one of these rules are allowed in Kickboxing?",
-        answer: "Kicks below the waist",
-        option: [
-            "Kicks above the elbow",
-            "Throwing punching in chest",
-            "Sweeping opponent to the ground",
-            "Kicks below the waist"
-        ]
+        a:  "Kicks above the elbow",
+        b:  "Throwing punching in chest",
+        c:  "Sweeping opponent to the ground",
+        d:  "Kicks below the waist",
+        correct: "d",
+           
+        
     },
 
     {
-        id: 2,
+        
         question: "Where did Kickboxing originate from?",
-        answer: "Southeast Asia",
-        option: [
-            "America",
-            "Germany",
-            "Australia",
-            "Southeast Asia"
-        ]
+        a:  "America",
+        b:  "Germany",
+        c:  "Australia",
+        d:  "Southeast Asia",
+        correct: "d",
+    
     },
 
     {
-        id: 3,
         question: "Which one is the benefit of Kickboxing?",
-        answer: "Self-Defense",
-        option: [
-            "More sleep",
-            "Hydration",
-            "Self-Defense",
-            "All of the above"
+        a:  "More sleep",
+        b:  "Hydration",
+        c:   "Self-Defense",
+        d:  "All of the above",
+        correct: "c",
 
-        ]
+        
     },
 
     {
-        id: 4,
         question: "Which of these is not a Kickboxing style?",
-        answer: "Firestarter",
-        option: [
-            "Muay Thai",
-            "Firestarter",
-            "Full Contact Karate",
-            "Savate"
-        ]
+        a:   "Muay Thai",
+        b: "Firestarter",
+        c:   "Full Contact Karate",
+        d:   "Savate",
+        correct: "b",
+        
     },
 
     {
-        id: 5,
         question: "Which country does Muay Thai orginates from?",
-        answer: "Thailand",
-        option: [
-            "Japan",
-            "China",
-            "Thailand",
-            "South Korea"
-        ]
+        a:    "Japan",
+        b:   "China",
+        c:   "Thailand",
+        d:   "South Korea",
+        correct: "c",
     },
 
     {
-        id: 6,
         question: "Who is the 2022 Heavyweight UFC Champion?",
-        answer: "Francis Ngannou",
-        option: [
-            "Francis Ngannou",
-            "Yoel Romero",
-            "Colby Covington",
-            "Kamaro Usman"
+        a:   "Francis Ngannou",
+        b:   "Yoel Romero",
+        c:   "Colby Covington",
+        d:   "Kamaro Usman",
+        correct: "a",
 
-        ]
+    
     },
 
     {
-        id: 7,
         question: "How many divisions are in the UFC?",
-        answer: "12",
-        option: [
-            "8",
-            "15",
-            "11",
-            "12"
+        a:    "8",
+        b:    "15",
+        c:   "11",
+        d:   "12",
+        correct: "d",
 
-        ]
+        
     },
 
     {
-        id: 8,
         question: "When year was the UFC founded?",
-        answer: "1993",
-        option: [
-            "1993",
-            "1972",
-            "1985",
-            "1991"
-        ]
+        a:    "1993",
+        b:   "1972",
+        c:   "1985",
+        d:    "1991",
+        correct: "a",
+        
     },
 
     {
-       id: 9,
        question: "Which one is considered a mixed martial arts (MMA)?",
-       answer: "Hybrid combat sport",
-       option: [
-           "Taekwondo",
-           "Karate",
-           "Hybrid combat sport",
-           "Judo"
-       ]
+        a:   "Taekwondo",
+        b:   "Karate",
+        c:   "Hybrid combat sport",
+        d:   "Judo",
+        correct: "c",
+    
     },
 
     {
-        id: 10,
         question: "Which is the main category of Martial arts?",
-        answer: "All of the above",
-        option: [
-            "Japanese",
-            "Chinese",
-            "Korean",
-            "All of the above"
-        ]
-    }
+        a:    "Japanese",
+        b:   "Chinese",
+        c:    "Korean",
+        d:    "All of the above",
+        correct: "d",
+    
+    },
 
-];
+]
+
+  const grabId = (idName) => {
+    
+    const ElementId = document.getElementById(idName);
+    console.log(ElementId, "<==idname")
+    if (ElementId) return ElementId;
+     // throw new Error(`cannot find the id ${idName}`);
+   
+  };
+  const option1 = grabId("option1");
+  const option2 = grabId("option2");
+  const option3 = grabId("option3");
+  const option4 = grabId("option4");
+  let nextBtn = grabId("btn-next");
+  let prevBtn = grabId("btn-prev");
+  const quiz = grabId("quiz");
+  const question = document.querySelector("h3");
+  const answers = document.querySelectorAll(".answer");
+  
+  let currentQuize = 0;
+  let score = 0;
+  
+  loadQuize();
+  function loadQuize() {
+  
+    unCheckAnswer();
+    let nextOption = Data[currentQuize];
+    console.log(nextOption, "next")
+    question.innerText = nextOption.question;
+    option1.innerText = nextOption.a;
+    option2.innerText = nextOption.b;
+    option3.innerText = nextOption.c;
+    option4.innerText = nextOption.d;
+    nextBtn.addEventListener("click", nextQuestion);
+    prevBtn.addEventListener("click", PreviousQuestion);
+  }
+  function nextQuestion() {
+    const answer = getValue();
+    console.log(quiz, "<===quiZ")
+    if (answer) {
+      if (answer === Data[currentQuize].correct) {
+        score++;
+      }
+      currentQuize++;
+      if (currentQuize < Data.length) {
+        loadQuize();
+      } else if (score === Data.length) {
+        console.log("you reached")
+        quiz.innerHTML = `<h1> Congratulations 👏👏 <br/>You scored ${score}/${Data.length}</h1>`;
+      } else {
+        quiz.innerHTML = `<h1> You scored ${score}/${Data.length}`;
+      }
+    }
+  }
+  
+  function PreviousQuestion() {
+    if (currentQuize.valueOf() === 0) {
+      alert("Can't go back anymore");
+    } else {
+      initialQuize--;
+      loadQuize();
+    }
+  }
+  
+  function getValue() {
+    let value = undefined;
+    answers.forEach((answer) => {
+      if (answer.checked) {
+        value = answer.id;
+      }
+    });
+    return value;
+  }
+  
+  function unCheckAnswer() {
+    answers.forEach((answer) => {
+      answer.checked = false;
+    });
+  }
+
+
+
+  // grab username & password, store to local storage as an object
+  //ex: {username: "balh", password: "fjkfjfgjk"}
+  //conditionally render the game.html ===> if username/password doesn't exist in LS, 
+  // redirect to homepage
+
+  //validate your inpurt fields for white spaces
 
